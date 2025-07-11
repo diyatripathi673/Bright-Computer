6c9e7612cec87d4eb4cd97987e1b510cd9a0cdb7
Diya Tripathi <tripathidiya673@gmail.com>
2025-07-11 23:07:23 +0530

    Backend Setup

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+import connectDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
 const PORT = 8080 || process.env.PORT;
-
+connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -19,17 +19,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// Connect to MongoDB
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("MongoDB connected");
-//     app.listen(PORT, () =>
-//       console.log(`Server running on http://localhost:${PORT}`)
-//     );
-//   })
-//   .catch((err) => console.error("MongoDB connection error:", err));
